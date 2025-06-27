a65ce46d2ebb7d4ac06a0c2ec172fd829e4daf3c
Kazi Mohibuzzaman <83270382+mohib051@users.noreply.github.com>
2025-06-27 11:45:59 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const _config = {
-    PORT: process.env.PORT || 8080,
-    DB_URL: process.env.DB_URL || 'mongodb+srv://mohib_051:DM3rXXellGvZvM4b@cluster0.ae6nd.mongodb.net/miniblog',
+    PORT: process.env.PORT,
+    DB_URL: process.env.DB_URL
 }
 
  const config = Object.freeze(_config);
